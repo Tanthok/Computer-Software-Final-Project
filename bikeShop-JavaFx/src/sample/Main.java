5a84134524267938b44ca0ada1f86261208d437a
jtovar2 <jtovar2@student.gsu.edu>
2016-11-04 14:48:51 -0400

    creating the front page of the javafx app

--- bikeShop-JavaFx/src/sample/Main.java
+++ bikeShop-JavaFx/src/sample/Main.java
@@ -4,16 +4,20 @@ import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.controlsfx.control.GridView;
+import org.controlsfx.control.cell.ImageGridCell;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -22,27 +26,72 @@ import java.io.File;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
 
+        BorderPane bigPane = new BorderPane();
 
+        String[] pathToImages = {"images/crank.jpg", "images/fork.png", "images/bicycle.jpg", "images/handlebar.jpg", "images/seat.jpg", "images/wheel.jpg"};
 
-        Group group = new Group();
-        BufferedImage buffImage = ImageIO.read(getClass().getResourceAsStream("images/crank.jpg"));
-        Image crank2 = SwingFXUtils.toFXImage(buffImage, null);
-        Image crank = new Image("file:images/crank.jpg", true);
-        Image fork = new Image("file:images/fork.png", true);
+        FlowPane flowPane = buildFlowPane(pathToImages, 300, 300);
 
-        ImageView img1 = new ImageView();
-        img1.setImage(crank2);
+        bigPane.setLeft(addVBox());
+        bigPane.setCenter(flowPane);
 
-        group.getChildren().add(img1);
 
         primaryStage.setTitle("hey hey");
-        primaryStage.setScene(new Scene(group));
+        primaryStage.setScene(new Scene(bigPane));
         primaryStage.show();
 
 
 
+    }
+
+    public FlowPane buildFlowPane(String[] pathToImages, int width, int height) throws Exception
+    {
+        FlowPane flowPane = new FlowPane();
+        flowPane.setPadding(new Insets(5, 0, 5, 0));
+        flowPane.setVgap(4);
+        flowPane.setHgap(4);
+        flowPane.setPrefWrapLength(1000);
+        flowPane.setStyle("-fx-background-color: DAE6F3;");
+
+        for(String pathToImage: pathToImages)
+        {
+            BufferedImage buffImage = ImageIO.read(getClass().getResourceAsStream(pathToImage));
+            Image image = SwingFXUtils.toFXImage(buffImage, null);
+            ImageView imageView = new ImageView();
+            imageView.setFitHeight(height);
+            imageView.setFitWidth(width);
+            imageView.setImage(image);
+            flowPane.getChildren().add(imageView);
+
+        }
+
+        return flowPane;
+    }
+
+    private VBox addVBox() throws Exception {
+
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10)); // Set all sides to 10
+        vbox.setSpacing(8);              // Gap between nodes
+
+        //Text title = new Text("Data");
+        //vbox.getChildren().add(title);
+
+        Hyperlink options[] = new Hyperlink[] {
+                new Hyperlink("Cart"),
+                new Hyperlink("User"),
+                new Hyperlink("Login")};
+
+        for (int i=0; i<3; i++) {
+            // Add offset to left side to indent from title
+            VBox.setMargin(options[i], new Insets(0, 0, 0, 8));
+            vbox.getChildren().add(options[i]);
+        }
+
+        return vbox;
     }
 
 
